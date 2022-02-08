36df304275a42809181f4b99a9ff26cc103d5a06
Frank <Franklinstevennealjr@gmail.com>
2022-02-07 17:50:09 -0700

    I love Coding Dojo

---
+++ for_loop_basic1.py
@@ -0,0 +1,23 @@
+for i in range(151):
+    print(i)
+
+for x in range(1001):
+    if x % 5 ==0:
+        print (x)
+
+for y in range(1, 101):
+    
+    if y % 10 ==0:
+        print ("Coding Dojo")
+    elif y % 5==0:
+        print ("Coding")
+    elif y % 1==0:
+        print (y)
+
+z = 0
+sum = 0
+while z<=500000:
+    if z % 2 ==1:
+        sum = sum+z
+        z = z+1
+        print (sum)
---
+++ practice.py
@@ -0,0 +1,7 @@
+z = 1 
+sum = 0
+while z <=50:
+    if z % 2 ==1:
+        sum = sum + z
+        z=z+1
+        print (sum)
