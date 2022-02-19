d2117e466e9d8fdfffd21f67eef8d861550eb738
Frank <Franklinstevennealjr@gmail.com>
2022-02-18 18:15:12 -0700

    User

--- banking.py
+++ banking.py
@@ -1,13 +1,7 @@
 class User:
-    bank_name = "Franklins Bank Foo"
+    bank_name = "Dojo Financial Institution"
     def __init__(self, name, email_address):
         self.name = name
         self.email_address = email_address
         self.account_balance = 0
-
-guido = User("gudio", "gueeds@python.org")
-monty = User("monty", "soypython.grr")
-
-print(guido.name,"  ", guido.email_address)
-print(monty.name)
 
