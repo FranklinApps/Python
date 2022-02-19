a112de0308e644c9a07dc63ed637b9a2bd4bb566
Frank <Franklinstevennealjr@gmail.com>
2022-02-19 08:02:16 -0700

    chaining

--- banking.py
+++ banking.py
@@ -7,17 +7,20 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdrawl(self, amount):
         self.account_balance -= amount
-
+        return self
+    
     def display_user_balance(self):
         print(f"Hello {self.name}, Your current account balance is {self.account_balance}")
+        return self
 
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance+=amount
-        
+        return self
 
 guido = User ("Guido Cheetos", "Gueeds@Cheese.com")
 
@@ -25,22 +28,11 @@ patrick= User("Patrick Star", "imastar@spongbob.com")
 
 aaron= User("Aaron Carter", "ImInDebt@Massivley.com")
 
-guido.make_deposit(100)
-guido.make_deposit(400)
-guido.make_withdrawl(75)
-guido.display_user_balance()
+guido.make_deposit(100).make_deposit(400).make_withdrawl(75).display_user_balance()
 
-patrick.make_deposit(85)
-patrick.make_deposit(185)
-patrick.make_withdrawl(30)
-patrick.make_withdrawl(20)
-patrick.display_user_balance()
+patrick.make_deposit(85).make_deposit(185).make_withdrawl(30).make_withdrawl(20).display_user_balance()
 
-aaron.make_deposit(25000000)
-aaron.make_withdrawl(16000000)
-aaron.make_withdrawl(14000000)
-aaron.make_withdrawl(6000000)
-aaron.display_user_balance()
+aaron.make_deposit(25000000).make_withdrawl(16000000).make_withdrawl(14000000).make_withdrawl(6000000).display_user_balance()
 
 patrick.transfer_money(guido, 20)
 
